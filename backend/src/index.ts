b86087d3bd6976bc4c85231bccb90b5005019d38
Elijah Arhinful <elijaharhinful8@gmail.com>
2025-06-17 19:49:21 +0000

    fix: fixed environment varibales not loading properly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
 
 // Import routes
 import authRoutes from './routes/auth';
 import movieRoutes from './routes/movies';
 import favouriteRoutes from './routes/favourites';
 
-dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,13 +21,19 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/movies', movieRoutes);
-app.use('/api/favourites', favouriteRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movies', movieRoutes);
+app.use('/api/v1/favourites', favouriteRoutes);
 
 // Root api
 app.get('/', (req, res) => {
+  res.json({ message: 'Pellicula API is running! but use this as the correct endpoint /api/v1' });
+});
+app.get('/api/v1', (req, res) => {
   res.json({ message: 'Pellicula API is running!' });
+});
+app.get('/api', (req, res) => {
+  res.json({ message: 'Pellicula API is running! but use this as the correct endpoint /api/v1' });
 });
 
 // MongoDB connection
